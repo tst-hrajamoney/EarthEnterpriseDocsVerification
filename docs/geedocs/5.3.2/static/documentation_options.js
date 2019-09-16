99b72cde153a35d4988af660ff12916e111295c3
Hemalatha Rajamoney <hrajamoney@t-sciences.com>
2019-09-16 14:11:19 -0700

    5.3.2 release branch changes

--- docs/geedocs/5.3.2/static/documentation_options.js
+++ docs/geedocs/5.3.2/static/documentation_options.js
@@ -4,6 +4,6 @@ var DOCUMENTATION_OPTIONS = {
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
-    HAS_SOURCE: false,
+    HAS_SOURCE: true,
     SOURCELINK_SUFFIX: '.txt'
 };
